83491e2469df6446444c97b7f6e21f966228b5a7
JaozaHaqq <109569563+JaozaHaqq@users.noreply.github.com>
2022-12-17 07:21:21 +0700

    Update Task.js

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
 const url = "YOURDEPLOYURL" // TODO: replace this
-const studentName = "JAOZA HAQQUESDA DAFFA" // TODO: replace this
+const studentName = "Jaoza Haqquesda Daffa" // TODO: replace this
 const studentId = "FE3191759" // TODO: replace this
 
 export { url, studentName, studentId }
